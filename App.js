4b256cde720437099ee0351d4e619ccf3970c600
FerFabianReyes <2100721a@umich.mx>
2025-09-14 23:06:37 -0600

    fondo cambiado a azul

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 import TranslatorScreen from './src/screens/TranslatorScreen';
+import { COLORS } from './src/styles/colors';
 
 export default function App() {
   return (
     <>
-      <StatusBar backgroundColor="#be3b97ff" barStyle="light-content" />
+      <StatusBar backgroundColor= {COLORS.primary}  barStyle="light-content" />
       <TranslatorScreen />
     </>
   );
