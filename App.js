2e0f426d1da1786f1857d84424cbce0d02576a51
FerFabianReyes <2100721a@umich.mx>
2025-10-13 00:10:52 -0600

    Las pantallas ya no se suben al salir de la aplicación

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
 import { COLORS } from './src/styles/colors';
 
 export default function App() {
   return (
-    <>
-      <StatusBar backgroundColor={COLORS.primary} barStyle="light-content" />
-      <AppNavigator />
-    </>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.primary }}>
+        <StatusBar backgroundColor={COLORS.primary} barStyle="light-content" />
+        <AppNavigator />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
